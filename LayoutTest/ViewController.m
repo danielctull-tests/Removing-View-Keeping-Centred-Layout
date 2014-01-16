e0059125571f1b40a075b28df18d564edf2148bc
Daniel Tull <dt@danieltull.co.uk>
2014-01-16 13:49:08 +0000

    Fully animate the removal

--- LayoutTest/ViewController.m
+++ LayoutTest/ViewController.m
@@ -10,14 +10,19 @@
 
 @interface ViewController ()
 @property (nonatomic, weak) IBOutlet UIView *topView;
+@property (nonatomic, weak) IBOutlet UIView *centeredView;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *constraint;
 @end
 
 @implementation ViewController
 
 - (IBAction)removeView:(id)sender {
-	[self.topView removeFromSuperview];
+	[self.centeredView removeConstraint:self.constraint];
 	[UIView animateWithDuration:3.0f animations:^{
+		self.topView.alpha = 0.0f;
 		[self.view layoutIfNeeded];
+	} completion:^(BOOL finished) {
+		[self.topView removeFromSuperview];
 	}];
 }
 
