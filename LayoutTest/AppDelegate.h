1d396feb2e41814bdbb8b1bc8df43a40d8c2c270
Daniel Tull <dt@danieltull.co.uk>
2014-01-16 13:15:12 +0000

    Initial Commit

---
+++ LayoutTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LayoutTest
+//
+//  Created by Daniel Tull on 16.01.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
